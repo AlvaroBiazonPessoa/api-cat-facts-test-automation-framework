90521a15e9fd29f113e2671fe02695a571b331c0
alvaro.b.pessoa <alvaro.b.pessoa@ericssoninovacao.com.br>
2024-10-02 18:40:42 -0300

    feat: Adding function getListOfBreedsWithoutAuthentication() to class TestCatFacts

--- src/test/java/TestCatFacts.java
+++ src/test/java/TestCatFacts.java
@@ -1,3 +1,8 @@
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
 public class TestCatFacts {
 
     private static final String URL_API_CAT_FACTS = "https://catfact.ninja/";
@@ -6,6 +11,13 @@ public class TestCatFacts {
     private static final String VALID_AUTHENTICATION_TOKEN_VALUE = "KqTRMzyCERr6njfNmmI2fGpW5otfuvL0IG1RB4th";
     private static final String QUERY_PARAMETER_LIMIT = "limit";
 
-
-
+    @Test
+    @DisplayName("Get list of breeds without authentication")
+    public void getListOfBreedsWithoutAuthentication() {
+        given()
+        .when()
+            .get(URL_API_CAT_FACTS + ENDPOINT_BREEDS)
+        .then()
+            .statusCode(HttpStatus.SC_UNAUTHORIZED);
+    }
 }
