3b1eea15e30be3a1a3ba888c2d5602a595cd9655
alvaro.b.pessoa <alvaro.b.pessoa@ericssoninovacao.com.br>
2024-10-02 18:20:13 -0300

    feat: Adding attributes that contain URL, endpoint, authentication credentials and name parameters in the TestCatFacts class

--- src/test/java/TestCatFacts.java
+++ src/test/java/TestCatFacts.java
@@ -1,3 +1,11 @@
 public class TestCatFacts {
 
+    private static final String URL_API_CAT_FACTS = "https://catfact.ninja/";
+    private static final String ENDPOINT_BREEDS = "breeds";
+    private static final String VALID_AUTHENTICATION_TOKEN_NAME = "X-CSRF-TOKEN";
+    private static final String VALID_AUTHENTICATION_TOKEN_VALUE = "KqTRMzyCERr6njfNmmI2fGpW5otfuvL0IG1RB4th";
+    private static final String QUERY_PARAMETER_LIMIT = "limit";
+
+
+
 }
